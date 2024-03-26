00b43d9f8327d3d7dbe77d3ed2e8513dff882474
ハムスケ ハム <daiki.esdfth12488@gmail.com>
2024-03-26 23:12:51 +0900

    something went wrong

---
+++ src/main/java/com/hamusuke/threadr/game/card/ServerCard.java
@@ -0,0 +1,16 @@
+package com.hamusuke.threadr.game.card;
+
+import com.hamusuke.threadr.network.Spider;
+import com.hamusuke.threadr.server.network.ServerSpider;
+
+public record ServerCard(ServerSpider owner, byte num) implements NumberCard {
+    @Override
+    public Spider getOwner() {
+        return this.owner;
+    }
+
+    @Override
+    public byte getNumber() {
+        return this.num;
+    }
+}
